15e45f009fdff63cd858b5e1c3e22dd95cc2b404
João Guilherme <36928653+Guilherme2041@users.noreply.github.com>
2023-05-13 11:05:30 -0300

    Production feature 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Básico IT Talent - Mudanças production dia 13-05-2023
+          DevOps Básico IT Talent - Mudanças production dia 13-05-2023 feature 2
         </p>
         <a
           className="App-link"
