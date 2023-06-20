b7c9d90b1fb297f37b3afafaef266fa266645063
João Guilherme <36928653+Guilherme2041@users.noreply.github.com>
2023-06-20 17:26:28 -0300

    Feature 434

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps Básico IT Talent - Mudanças dia 20-06-2023 feature 433 production
+          DevOps Básico IT Talent - Mudanças dia 20-06-2023 feature 434 production
         </p>
         <a
           className="App-link"
